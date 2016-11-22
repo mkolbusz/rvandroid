fb828077af3a354aefa2a90bd387fe380f4a6fc8
Michal Kolbusz <michalkolbusz@gmail.com>
2016-11-23 00:52:22 +0100

    Created Adapter class

---
+++ app/src/main/java/pl/edu/agh/mkolbusz/recyclerview/MovieRVAdapter.java
@@ -0,0 +1,27 @@
+package pl.edu.agh.mkolbusz.recyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by mkolbusz on 23.11.16.
+ */
+
+public class MovieRVAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+}
